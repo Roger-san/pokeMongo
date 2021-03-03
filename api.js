af26755254e41acbf2ac0b29c7d60eca8667821d
Roger <rogerytal@gmail.com>
2021-03-03 23:22:18 +0100

    fin dia 1

--- api.js
+++ api.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-const fs = require("fs")
 
-const database = "./db/database.json"
 const Pokemon = require("./models/Pokemon")
 const URL = "mongodb://localhost/Pokemons"
 
@@ -40,117 +38,89 @@ api.get("/api/pokemon", (req, res) => {
   })
 })
 
-api.post("/api/pokemons", (req, res) => {
-  if (!request.body.name || !request.body.type || !request.body.locations) {
-    response.status(400).send({
-      success: false,
-      url: "/api/pokemons",
-      method: "post",
-      message: "fallo faltan mas datos",
+api.get("/api/pokemons/:id", (req, res) => {
+  Pokemon.findById(req.params.id, (err, pokemon) => {
+    if (err) console.error(err)
+    else res.send(pokemon)
+  })
+})
+
+api.get("/api/pokemons/:id/locations/:locationId", (req, res) => {
+  Pokemon.findById(req.params.id, (err, poke) => {
+    if (err) console.error(err)
+    else poke.findOne({"locations.id": req.params.locationId})
+  })
+})
+
+api.get("/api/pageoffset/pokemons/", (request, response) => {
+  fs.readFile(database, (error, data) => {
+    if (error) throw error
+    const allPokemons = JSON.parse(data)
+    const n1 = Number.parseInt(request.query.offset) - 1
+    const n2 = Number.parseInt(request.query.limit)
+
+    const list = allPokemons.slice(n1, n1 + n2)
+    response.status(200).send({
+      success: true,
+      url: "/api/pokemons/pageoffset/",
+      method: "get",
+      message: list,
     })
-  } else {
-    fs.readFile(database, (error, data) => {
-      if (error) throw error
+  })
+})
 
-      const allPokemons = JSON.parse(data)
-      const newPokemon = {
-        id: allPokemons.length + 1,
-        name: request.body.name,
-        type: request.body.type,
-      }
-      allPokemons.push(newPokemon)
-
-      fs.writeFile(database, JSON.stringify(allPokemons), (error) => {
-        if (error) {
-          response.status(400).send({
-            success: false,
-            url: "/api/pokemons",
-            method: "post",
-            message: "fallo",
-          })
-        } else {
-          response.status(200).send({
-            success: true,
-            url: "/api/pokemons",
-            method: "post",
-            message: newPokemon,
-          })
-        }
+api.delete("/api/pokemons", (req, res) => {
+  Pokemon.exists({_id: req.body.id}).then((boolean) => {
+    if (!boolean) res.send("pokemon no existe")
+    else
+      Pokemon.findByIdAndDelete(req.body.id, (err, data) => {
+        if (err) console.error(err)
+        res.send("pokemon borrado correctamente")
       })
-    })
+  })
+})
+
+api.post("/api/pokemons", (req, res) => {
+  if (!req.body.name || !req.body.type || !req.body.locations) {
+    res.send("more data is needed")
+  } else {
+    Pokemon.create(
+      {
+        name: req.body.name,
+        type: req.body.type,
+        locations: req.body.locations,
+      },
+      (err, data) => {
+        if (err) res.send(err)
+        else res.send(data)
+      }
+    )
   }
 })
 
-// api.delete("/api/pokemons", (request, response) => {
-//   if (!request.body.name) {
-//     response.status(400).send({
-//       success: false,
-//       url: "/api/pokemons",
-//       method: "delete",
-//       message: "hace falta el nombre",
-//     })
-//   } else {
-//     fs.readFile(database, (error, data) => {
-//       if (error) throw error
-
-//       const allPokemons = JSON.parse(data)
-//       const newPokemons = allPokemons.filter((x) => !(x.name === request.body.name))
-//       fs.writeFile(database, JSON.stringify(newPokemons), (error) => {
-//         if (error) {
-//           response.status(400).send({
-//             success: false,
-//             url: "/api/pokemons",
-//             method: "delete",
-//             message: "fallo",
-//           })
-//         } else {
-//           response.status(200).send({
-//             success: true,
-//             url: "/api/pokemons",
-//             method: "delete",
-//             Pokemons: allPokemons[0],
-//           })
-//         }
-//       })
-//     })
-//   }
-// })
-
-// api.put("/api/pokemons/:id", (request, response) => {
-//   fs.readFile(database, (error, data) => {
-//     const allPokemons = JSON.parse(data)
-
-//     const pokemons = allPokemons.map((x) => {
-//       if (x.id === request.params.id) {
-//         return (x = {
-//           id: x.id,
-//           name: request.body.name || x.name,
-//           type: request.body.type || x.type,
-//         })
-//       } else {
-//         return x
-//       }
-//     })
-
-//     fs.writeFile(database, JSON.stringify(pokemons), (error) => {
-//       if (error) {
-//         response.status(400).send({
-//           success: false,
-//           url: "/api/pokemons",
-//           method: "put",
-//           message: "fallo",
-//         })
-//       } else {
-//         response.status(200).send({
-//           success: true,
-//           url: "/api/pokemons",
-//           method: "put",
-//           Pokemons: pokemons,
-//         })
-//       }
-//     })
-//   })
-// })
+api.put("/api/pokemons/:id", (req, res) => {
+  Pokemon.exists({_id: req.params.id}).then((boolean) => {
+    if (!boolean) res.send("pokemon no existe")
+    else if (!req.body.name || !req.body.type || !req.body.locations) {
+      res.send("more data is needed")
+    } else {
+      const newPokemon = {
+        name: req.body.name,
+        type: req.body.type,
+        locations: req.body.locations,
+      }
+      const opts = {
+        new: true,
+        runValidators: false,
+        useFindAndModify: false,
+      }
+      Pokemon.findByIdAndUpdate(req.params.id, newPokemon, opts, (err, data) => {
+        if (err) res.send(err)
+        res.send(data) //problema con el id de locations
+      })
+    }
+  })
+})
 
 // api.get("/api/pokemons/page/:pag", (request, response) => {
 //   fs.readFile(database, (error, data) => {
@@ -180,69 +150,4 @@ api.post("/api/pokemons", (req, res) => {
 //   })
 // })
 
-// api.get("/api/pageoffset/pokemons/", (request, response) => {
-//   fs.readFile(database, (error, data) => {
-//     if (error) throw error
-//     const allPokemons = JSON.parse(data)
-//     const n1 = Number.parseInt(request.query.offset) - 1
-//     const n2 = Number.parseInt(request.query.limit)
-
-//     const list = allPokemons.slice(n1, n1 + n2)
-//     response.status(200).send({
-//       success: true,
-//       url: "/api/pokemons/pageoffset/",
-//       method: "get",
-//       message: list,
-//     })
-//   })
-// })
-
-// api.get("/api/pokemons/:id/locations/:locationId", (request, response) => {
-//   fs.readFile(database, (error, data) => {
-//     if (error) throw error
-//     const {id, locationId} = request.params
-//     const allPokemons = JSON.parse(data)
-//     const pokemonLoc = allPokemons.filter((x) => x.id === id)
-//     const location = pokemonLoc[0].locations.filter((x) => x.id === locationId)
-//     console.log(pokemonLoc)
-//     response.status(200).send({
-//       success: true,
-//       url: "/api/pokemons/{id}/locations/{locationId}",
-//       method: "get",
-//       pokemon: pokemonLoc[0].name,
-//       location: location[0],
-//     })
-//   })
-// })
-
-// api.get("/api/pokemons", (request, response) => {
-//   fs.readFile(database, (error, data) => {
-//     if (error) throw error
-
-//     const allPokemons = JSON.parse(data)
-
-//     response.status(200).send({
-//       success: true,
-//       url: "/api/pokemons",
-//       method: "get",
-//       pokemons: allPokemons,
-//     })
-//   })
-// })
-
-// api.get("/api/pokemons/:id", (request, response) => {
-//   fs.readFile(database, (error, data) => {
-//     const allPokemons = JSON.parse(data)
-//     const pokemon = allPokemons.filter((x) => x.id === request.params.id)
-//     response.status(200).send({
-//       success: true,
-//       url: "/api/pokemons/",
-//       method: "get",
-//       pokemon: pokemon[0],
-//     })
-//   })
-// })
-
-api.listen(1013, () => {
-  console.log("api funcionando en el localhost:1013")
-})
+api.listen(1013, () => console.log("server corriendo en localhost:1013"))
